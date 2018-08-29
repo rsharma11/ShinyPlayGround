3054b52435495dee0279c9b7d5928086a627e2bd
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-08-30 01:42:15 +0200

    More_Action_Buttons

--- Action Buttons/app.R
+++ Action Buttons/app.R
@@ -7,31 +7,68 @@ ui <- (fluidPage(
   sidebarLayout(
     sidebarPanel(
       br(),
-      h4("Actionbutton"),
-      actionButton(inputId = "per", label = "perform"),
+      fluidRow(
+        column(6,
+               h4("Actionbutton"),
+               actionButton("per", label = "Perform")),
+        column(6,
+               h4("Help Text"),
+               helpText("Just for help"))),
       br(),
-      h4("Submitbutton"),
-      submitButton(text = "Submit"),
-      h4("Single Checkbox"),
-      checkboxInput(inputId = "checkbox", label = "Choice A", value = TRUE),
-      checkboxGroupInput(inputId = "checkGroup", label = h4("Checkbox group"), choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), selected = 2),
-      dateInput(inputId = "date", label = h4("Date Input"), value = "2016-12-01"),
-      dateRangeInput(inputId = "dates", label = h4("Date Range")),
-      fileInput(inputId = "file", label = h4("File Input"))      
-    ),
-    mainPanel(
-      h2("Main"),
-      br(),
-      br(),
-      tags$img(src="Petal.jpg", height =150, width = 200),
-      br(),
-      br(),
-        p(HTML("This famous (Fisher’s or Anderson’s) iris data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are <b>Iris setosa</b>, <b>versicolor</b>, and <b>virginica</b>. Use an HTML tag format.")),
-        br(),  
-        a("http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html"),
-        h2("Analyis")
-      
-        )))))
+      fluidRow(
+        column(6,
+               h4("Submitbutton"),
+               submitButton("Submit")),
+        column(6,
+               numericInput("numer",
+                            label = h4("Numeric Input"),
+                            value = 10))),
+      fluidRow(
+        column(6,
+               h4("Single Checkbox"),
+               checkboxInput("checkbox", label = "Choice A", value = TRUE)),
+        column(6,
+               radioButtons("radio", label = h4("Radio Buttons"),
+                            choices = list("Choice 1" = 1, "Choice 2" = 2),
+                            selected = 2))),
+      fluidRow(
+        column(6,
+               checkboxGroupInput("checkGroup",
+                                  label = h4("Checkbox group"),
+                                  choices = list("Choice 1" = 1,
+                                                 "Choice 2" = 2, "Choice 3" = 3),
+                                  selected = 2)),
+        
+        column(6,
+               selectInput("select", label = h4("Select Box"),
+                           choices = list("Choice 1" = 1, "Choice 2" = 2
+                           ), selected = 1))),
+      fluidRow(
+        column(6,
+               dateInput("date",
+                         label = h4("Date input"),
+                         value = "2016-12-01")),
+        column(6,
+               sliderInput("slider1", label = h4("Sliders"),
+                           min = 0, max = 100, value = c(10,90)))),
+      fluidRow(
+        column(6,
+               dateRangeInput("dates", label = h4("Date Range"))),
+        column(6,
+               textInput("text", label = h4("Text Input"),
+                         value = "Some Text"))),
+      fileInput("file", label = h4("File Input"))),
+    mainPanel(h1("Main"),
+              img(src = "petal.jpg", height = 150, width = 200),
+              br(),
+              br(),
+              p("This famous (Fisher's or Anderson's) ", a("iris",href="http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html"), "data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are ",strong( "Iris setosa,"),strong( "versicolor"), "and", strong("virginica.")),
+              br(),
+              h2("Analysis")
+              
+    )
+  )
+  )))
 server <- function(input, output, session) {}
 
 shinyApp(ui = ui, server = server)
