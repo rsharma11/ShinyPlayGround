c6cfe6ddf7f009c7bc14ff99369fa660b640906f
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-09-11 22:35:26 +0200

    AlishaTracker

--- AlishaGrowthTracker/server.R
+++ AlishaGrowthTracker/server.R
@@ -1,27 +1,65 @@
-
 shinyServer(function(input, output) {
-    #Run this code when action button (submit) is presssed
-    observeEvent(input$submit,{
+
+    #Run this code when action button (submit_food) is presssed
+    observeEvent(input$submit_food,{
       #creating data object from user input
-      data <- data.frame(
-        date = input$date,
-        time= input$time2,
+      data_food <- data.frame(
+        date = input$date_food,
+        time= input$time_food,
         milk = input$milk
       )
       #push data into mongo
-      writeInMongo(data, "Milk")      
+      writeInMongo(data_food, "Food")      
       
     })
     
+  #Run this code when action button (submit_diaper) is presssed
+  observeEvent(input$submit_diaper,{
+    #creating data object from user input
+    data_diaper <- data.frame(
+      date = input$date_diaper,
+      time= input$time_diaper,
+      diaper = input$diaper
+    )
+    #push data into mongo
+    writeInMongo(data_diaper, "Diaper")      
     
-    #ObserveEvent----
-    #Run this code when action button (submit) is presssed
-    observeEvent(input$createTable,{
-      
-      #push data into mongo
-      data <- readFromMongo("Milk")      
-      output$table <- renderDataTable({
-        DT::datatable(data)
+  })
+  
+  #Run this code when action button (submit_sleep) is presssed
+  observeEvent(input$submit_sleep,{
+    #creating data object from user input
+    data_sleep <- data.frame(
+      date = input$date_sleep,
+      Slept = input$time1,
+      Woke = input$time2
+    )
+    #push data into mongo
+    writeInMongo(data_sleep, "Sleep")      
+    
+  })
+    
+  #Run this code when action button (create table) is presssed
+  
+   observeEvent(input$createTable_food,{
+    #read data into mongo
+    data_food <- readFromMongo("Food")      
+    output$table_food <- renderDataTable({
+      DT::datatable(data_food)
+    })
+  })
+  
+    observeEvent(input$createTable_diaper,{
+      data_diaper <- readFromMongo("Diaper")      
+      output$table_diaper <- renderDataTable({
+        DT::datatable(data_diaper)
+      })
+    })
+    
+    observeEvent(input$createTable_sleep,{
+      data_sleep <- readFromMongo("Sleep")      
+      output$table_sleep <- renderDataTable({
+        DT::datatable(data_sleep)
       })
     })
     
--- AlishaGrowthTracker/ui.R
+++ AlishaGrowthTracker/ui.R
@@ -2,47 +2,63 @@ shinyUI(
 dashboardPage(
     dashboardHeader(title = "Alisha's Activity Tracker"),
     dashboardSidebar(
-      sidebarMenu(
+      sidebarMenu(id = "sidebarmenu",
         menuItem("Activity", tabName = "Activity", icon = icon("Activity")),
-        menuItem("Health details", tabName = "Health details", icon = icon("Health details")),
-        menuItem("View Plot", tabName = "View Plot", icon = icon("View Plot"))
+        menuItem("Health details", tabName = "details", icon = icon("Health details")),
+        menuItem("View Plot", tabName = "ViewPlot", icon = icon("View Plot"))
       )
     ),
+    
     dashboardBody(
-            fluidPage(
-                mainPanel(
-                    tabsetPanel(
-                          tabPanel("Food", value = "food", dateInput("date", label = "Date:"),
-                                  timeInput("time2", "Time:", value = Sys.time()),
-                                  numericInput("milk", label = "Milk quantity (ml):", value = 170,  min = 0, max = 300, step = 10),
-                                  selectInput("food", 
-                                              "Food eaten (gm):",
-                                              c("Fruits" = "fruits",
-                                                "Vegetable" = "vegetable",
-                                                "Cereals" = "cereal")),
-                                   actionButton("submit", label = "Submit"),
-                                   actionButton("createTable", "Create table"),
-                                   dataTableOutput("table")),
-      
-                          tabPanel("Diaper", value = "Diaper", dateInput("date", label = "Date:"),
-                                   timeInput("time2", "Time:", value = Sys.time()),
-                                   selectInput("diaper", "Event happened:",
-                                               c("Soiled" = "big job",
-                                                 "Pee" = "small job",
-                                                 "Soiled_Pee" = "soiled_pee")),
-                                   actionButton("submit", label = "Submit"),
-                                   actionButton("createTable", "Create table"),
-                                   dataTableOutput("table")),
-      
-                          tabPanel("Sleep", value = "Sleep", dateInput("date", label = "Date:"),
-                                   timeInput("time3", "Slept at:", value = strptime("12:34:56", "%T")),
-                                   timeInput("time3", "Woke up at:", value = strptime("12:34:56", "%T")),
-                                   actionButton("submit", label = "Submit"),
-                                   actionButton("createTable", "Create table"),
-                                   dataTableOutput("table"))
-                              )
-                            )
-                          )
-                      )
-    )
+      tabItems(
+        tabItem(tabName = "Activity",
+                tabsetPanel(
+                  tabPanel("Food", value = "food", dateInput("date_food", label = "Date:"),
+                           timeInput("time_food", "Time:", value = Sys.time()),
+                           numericInput("milk", label = "Milk quantity (ml):", value = 170,  min = 0, max = 300, step = 10),
+                           selectInput("food", 
+                                       "Food eaten (gm):",
+                                       c("Fruits" = "fruits",
+                                         "Vegetable" = "vegetable",
+                                         "Cereals" = "cereal")),
+                           actionButton("submit_food", label = "Submit"),
+                           actionButton("createTable_food", "Create table"),
+                           dataTableOutput("table_food")),
+                  
+                  tabPanel("Diaper", value = "Diaper", dateInput("date_diaper", label = "Date:"),
+                           timeInput("time_diaper", "Time:", value = Sys.time()),
+                           selectInput("diaper", "Event happened:",
+                                       c("Soiled" = "big job",
+                                         "Pee" = "small job",
+                                         "Soiled_Pee" = "soiled_pee")),
+                           actionButton("submit_diaper", label = "Submit"),
+                           actionButton("createTable_diaper", "Create table"),
+                           dataTableOutput("table_diaper")),
+                  
+                  tabPanel("Sleep", value = "Sleep", dateInput("date_sleep", label = "Date:"),
+                           timeInput("time1", "Slept at:", value = strptime("12:34:56", "%T")),
+                           timeInput("time2", "Woke up at:", value = strptime("12:34:56", "%T")),
+                           actionButton("submit_sleep", label = "Submit"),
+                           actionButton("createTable_sleep", "Create table"),
+                           dataTableOutput("table_sleep"))
+                )
+        ),
+        tabItem(tabName = "details",
+                dateInput("Date", "Enter Date of appointment", value = ""),
+                textInput("height", "Enter Height", value = ""),
+                textInput("weight", "Enter Weight", value = ""),
+                textInput("vaccine", "Enter name of vaccine", value = ""),
+                textInput("vaccine", "Additional information", value = "")
+        ),
+        tabItem(tabName = "ViewPlot",
+                radioButtons("plot", "Type of plot:",
+                             c("Scatter" = "scatter",
+                               "Line" = "line",
+                               "Box plot" = "box",
+                               "Histogram" = "hist"), inline = TRUE),
+                plotOutput("Plot")
+        )
+        )
+)
+)
 )
